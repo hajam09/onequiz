d3f1530421ddae29b5c206d9ea3c628d8d2ccbd5
DESKTOP-9M5JTD4\HM <hajam09@yahoo.com>
2022-11-05 07:57:18 +0000

    model and settings setup

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,67 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
+from django.utils.translation import ugettext_lazy as _
 
-# Register your models here.
+from quiz.models import Answer
+from quiz.models import EssayQuestion
+from quiz.models import MultipleChoiceQuestion
+from quiz.models import Question
+from quiz.models import Quiz
+from quiz.models import Result
+from quiz.models import Subject
+from quiz.models import Topic
+from quiz.models import TrueOfFalseQuestion
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+
+class QuizAdminForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        exclude = []
+
+    questions = forms.ModelMultipleChoiceField(
+        queryset=Question.objects.all().select_subclasses(),
+        required=False,
+        label=_("Questions"),
+        widget=FilteredSelectMultiple(
+            verbose_name=_("Questions"),
+            is_stacked=False)
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(QuizAdminForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['questions'].initial = self.instance.question_set.all().select_subclasses()
+
+    def save(self, commit=True):
+        quiz = super(QuizAdminForm, self).save(commit=False)
+        quiz.save()
+        quiz.question_set.set(self.cleaned_data['questions'])
+        self.save_m2m()
+        return quiz
+
+
+class QuizAdmin(admin.ModelAdmin):
+    form = QuizAdminForm
+
+    list_display = ('name', 'topic',)
+    list_filter = ('topic',)
+    search_fields = ('description', 'topic',)
+
+
+class MultipleChoiceQuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+admin.site.register(Subject)
+admin.site.register(Topic)
+admin.site.register(EssayQuestion)
+admin.site.register(MultipleChoiceQuestion, MultipleChoiceQuestionAdmin)
+admin.site.register(TrueOfFalseQuestion)
+admin.site.register(Answer)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(Result)
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,255 @@
-from django.db import models
+import random
+import re
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from model_utils.managers import InheritanceManager
+
+PERCENTAGE_VALIDATOR = [MinValueValidator(0), MaxValueValidator(100)]
+
+
+class BaseModel(models.Model):
+    createdDttm = models.DateTimeField(default=timezone.now)
+    modifiedDttm = models.DateTimeField(auto_now=True)
+    reference = models.CharField(max_length=2048, blank=True, null=True)
+    deleteFl = models.BooleanField(default=False)
+    orderNo = models.IntegerField(default=1, blank=True, null=True)
+    versionNo = models.IntegerField(default=1, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Subject(BaseModel):
+    # Maths, Science, Geography, ...
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(BaseModel):
+    """
+    Within the subject they could be tested in specific topic.
+    e.g -> Maths (Algebra, Probability, ...)
+    e.g -> Chemistry (Atomic structure, Chemical bonds, ...)
+    """
+    name = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subjectTopics')
+    description = models.TextField(blank=True, null=True)
+
+
+class Question(BaseModel):
+    figure = models.ImageField(blank=True, null=True, upload_to='uploads/%Y/%m/%d')
+    content = models.TextField()
+    explanation = models.TextField(max_length=2048, blank=True, null=True)
+    mark = models.SmallIntegerField(blank=True, null=True, default=1)
+
+    objects = InheritanceManager()
+
+
+class EssayQuestion(Question):
+    def checkIfCorrect(self, guess):
+        return False
+
+    def getAnswers(self):
+        return False
+
+    def getAnswersList(self):
+        return False
+
+    def answerChoiceToString(self, guess):
+        return str(guess)
+
+    def __str__(self):
+        return self.content
+
+
+class MultipleChoiceQuestion(Question):
+    class Order(models.TextChoices):
+        CREATION = 'CREATION', _('Creation')
+        RANDOM = 'RANDOM', _('Random')
+        NONE = 'NONE', _('None')
+
+    answerOrder = models.CharField(max_length=30, choices=Order.choices, default=Order.RANDOM)
+
+    def checkIfCorrect(self, pk):
+        answer = Answer.objects.get(id=pk)
+        return answer.isCorrect
+
+    def orderAnswers(self, queryset):
+        if self.answerOrder == self.Order.CREATION:
+            return queryset.order_by('id')
+        if self.answerOrder == self.Order.RANDOM:
+            return queryset.order_by('?')
+        if self.answerOrder == self.Order.NONE:
+            return queryset.order_by()
+        return queryset
+
+    def get_answers(self):
+        return self.orderAnswers(Answer.objects.filter(question=self))
+
+    def getAnswersList(self):
+        return [(answer.id, answer.content) for answer in self.orderAnswers(Answer.objects.filter(question=self))]
+
+    def answerChoiceToString(self, guess):
+        return Answer.objects.get(id=guess).content
+
+
+class TrueOfFalseQuestion(Question):
+    isCorrect = models.BooleanField(default=False)
+
+    def checkIfCorrect(self, guess):
+        if guess == "True":
+            guessBool = True
+        elif guess == "False":
+            guessBool = False
+        else:
+            return False
+
+        return guessBool == self.isCorrect
+
+    def getAnswers(self):
+        context = [
+            {'correct': self.checkIfCorrect("True"), 'content': 'True'},
+            {'correct': self.checkIfCorrect("False"), 'content': 'False'}
+        ]
+        return context
+
+    def getAnswersList(self):
+        return [(True, True), (False, False)]
+
+    def answerChoiceToString(self, guess):
+        return str(guess)
+
+
+class Answer(BaseModel):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    content = models.TextField()
+    isCorrect = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content
+
+    # def ifCorrect(self, userAnswer):
+    #     stripeText = re.sub(' +', ' ', self.content)
+    #     stripeUserAnswer = re.sub(' +', ' ', userAnswer)
+    #     return stripeText.casefold() == stripeUserAnswer.casefold()
+
+
+class Quiz(BaseModel):
+    """
+    The base of our app
+    """
+
+    class Difficulty(models.TextChoices):
+        EASY = 'EASY', _('Easy')
+        MEDIUM = 'MEDIUM', _('Medium')
+        HARD = 'HARD', _('Hard')
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    url = models.SlugField(max_length=255, blank=False)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, blank=True, related_name='topicQuizzes')
+    numberOfQuestions = models.PositiveIntegerField()
+    quizDuration = models.IntegerField()
+    difficulty = models.CharField(max_length=10, choices=Difficulty.choices, default=Difficulty.EASY)
+    passMark = models.SmallIntegerField(blank=True, default=0, validators=[MaxValueValidator(100)])
+    successText = models.TextField(blank=True, null=True)
+    failText = models.TextField(blank=True, null=True)
+    questions = models.ManyToManyField(Question, blank=True, related_name='quizQuestions')
+
+    inRandomOrder = models.BooleanField(default=False)
+    answerAtEnd = models.BooleanField(default=False)
+    isExamPaper = models.BooleanField(default=False)
+    isDraft = models.BooleanField(default=False)
+    maxAttempt = models.PositiveIntegerField(default=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save(self, force_insert=False, force_update=False, *args, **kwargs):
+        self.url = re.sub('\s+', '-', self.url).lower()
+        self.url = ''.join(letter for letter in self.url if letter.isalnum() or letter == '-')
+
+        if self.maxAttempt == 1:
+            self.isExamPaper = True
+
+        if self.passMark > 100:
+            raise ValidationError(f'{self.passMark} is above 100')
+
+        super(Quiz, self).save(force_insert, force_update, *args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name} - {self.topic.name}"
+
+    def getQuestions(self):
+        return self.questions.all().select_subclasses()
+
+    @property
+    def getQuestions(self):
+        questions = list(self.questions.all())
+        random.shuffle(questions)
+        return questions[:self.numberOfQuestions]
+
+    @property
+    def getMaxScore(self):
+        return self.getQuestions().count()
+
+
+# class ProgressManager(models.Manager):
+#
+#     def newProgress(self, user):
+#         newProgress = self.create(user=user, score='')
+#         newProgress.save()
+#         return newProgress
+#
+#
+# class Progress(BaseModel):
+#     """
+#        Progress is used to track an individual signed in users score on different
+#        quiz's and categories
+#        Data stored in csv using the format:
+#            category, score, possible, category, score, possible, ...
+#     """
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+# class Question(BaseModel):
+#     class AnswerType(models.TextChoices):
+#         TEXT = 'TEXT', _('Text')  # free text -> answer depends on the user.
+#         RADIO_BOX = 'RADIO_BOX', _('Radio box')  # radio box options -> single correct answer from multiple choices.
+#         CHECK_BOX = 'CHECK_BOX', _('Check box')  # check box options -> multiple correct answers from multiple choices
+#
+#     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='questions')
+#     figure = models.ImageField(blank=True, null=True, upload_to='uploads/%Y/%m/%d')
+#     content = models.TextField()
+#     answerType = models.CharField(max_length=64, choices=AnswerType.choices)
+#     explanation = models.TextField(max_length=2048, blank=True, null=True)
+#
+#     objects = InheritanceManager()
+#
+#     @property
+#     def getAnswers(self):
+#         return self.answers.all()
+
+
+# class Answer(BaseModel):
+#     content = models.TextField()
+#     isCorrect = models.BooleanField(default=False)
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+#     marks = models.SmallIntegerField(blank=True, null=True, default=1)
+
+# def ifCorrect(self, userAnswer):
+#     stripeText = re.sub(' +', ' ', self.content)
+#     stripeUserAnswer = re.sub(' +', ' ', userAnswer)
+#     return stripeText.casefold() == stripeUserAnswer.casefold()
+
+
+class Result(BaseModel):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.DecimalField(max_digits=5, decimal_places=2, default=0, validators=PERCENTAGE_VALIDATOR)
