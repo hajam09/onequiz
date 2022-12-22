0aeb9afc91217b615e24683f83932986319e77d3
Mohamed Ashraf Hajamohideen <34398691+hajam09@users.noreply.github.com>
2022-12-22 11:01:11 +0000

    Refactor quiz attempt view template (#7)
    Remove unused CDN from base template.
    Use production ready React CDN files.
    Refactor quiz/templates/quiz/quizAttemptView.html
    Show result view for marked attempts.
    Unit test for quizAttemptsForQuizView

--- quiz/views.py
+++ quiz/views.py
@@ -274,7 +274,6 @@ class AttemptedQuizzesView(ListView):
 @login_required
 def quizAttemptsForQuizView(request, quizId):
     quizAttemptList = QuizAttempt.objects.select_related('user').filter(quiz_id=quizId)
-    # types of actions: View this attempt, Mark this attempt / View results, Flag this attempt, Delete this attempt.
     context = {
         'quizAttemptList': quizAttemptList,
         'quizId': quizId,
